8d717fd6be6fc7f71779fa8a1d7df746c4968084
Amanda Treutler <amandathedev@gmail.com>
2019-02-06 14:07:17 -0600

    Guessing Game

--- number-methods.js
+++ number-methods.js
@@ -1,18 +1,18 @@
-let num = 102.824
+let num = 102.824;
 
 // Cusomize the amount of decimal places displayed
-console.log(num.toFixed(2))
+console.log(num.toFixed(2));
 
-
-console.log(Math.round(num))
+// Round
+console.log(Math.round(num));
 
 // Forces it to round down
-console.log(Math.floor(num))
+console.log(Math.floor(num));
 
 // Forces round up
-console.log(Math.ceil(num))
+console.log(Math.ceil(num));
 
-let min = 10
-let max = 20
-let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
-console.log(randomNum)
+let min = 10;
+let max = 20;
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(randomNum);
