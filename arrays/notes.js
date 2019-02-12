7596d114e0fc573ac8f95a5f376069d329417fa8
Amanda Treutler <amandathedev@gmail.com>
2019-02-12 07:33:53 -0600

    Searching arrays

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,35 @@
-const notes = ["Note 1", "Note 2", "Note 3"];
+const todo = [
+  {
+    text: "Buy new kettle",
+    completed: false
+  },
+  {
+    text: "Call doctor",
+    completed: true
+  },
+  {
+    text: "Get oil change",
+    completed: false
+  },
+  {
+    text: "Hang pictures",
+    completed: true
+  },
+  {
+    text: "Call mom",
+    completed: true
+  }
+];
 
-console.log(notes.length);
-console.log(notes[2]);
+const deleteTodo = function(todo, todoText) {
+  const index = todo.findIndex(function(todo) {
+    return todo.text.toLowerCase() === todoText.toLowerCase();
+  });
+
+  if (index > -1) {
+    todo.splice(index, 1);
+  }
+};
+
+// console.log(notes.length);
+// console.log(notes[2]);
